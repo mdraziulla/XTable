e17f5cfe20052782acb55208b9775f3779aa3d7f
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-30 22:21:52 +0530

    Date and Views Table

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import DataViewTable from './XTable'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DataViewTable />
+      </div>
+    </>
+  )
+}
+
+export default App
